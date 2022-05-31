f710976c687498ed7bd377404618e11d1449800c
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-05-31 23:56:57 +0200

    Removed unused variable from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import LoginForm from "./components/LoginForm/LoginForm";
-import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
   return (
     <>
       <h1>Pizzadvisor</h1>
       <LoginForm />
-      {/* <RegisterForm /> */}
     </>
   );
 }
