252639195d56db9f24c5e4177b0423a8eee2ab18
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-01 20:55:20 +0200

    Added a login Form test

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -5,6 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store";
 import LoginForm from "./LoginForm";
 
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
 describe("Given a LoginForm Component", () => {
   describe("When it's invoked", () => {
     test("Then it should render 2 input fields and 2 buttons", () => {
@@ -82,6 +89,31 @@ describe("Given a LoginForm Component", () => {
       userEvent.type(passwordInput, password);
 
       expect(registerButton).not.toBeDisabled();
+    });
+  });
+  describe("When the user fill the username and password input fields and the user clicks on the login button", () => {
+    test("Then the dispatch should be invoked", () => {
+      const username = "lelo";
+      const password = "lelo";
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <LoginForm />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const usernameInput = screen.getByLabelText("Username");
+      const passwordInput = screen.getByLabelText("Password");
+      const loginButton = screen.getByRole("button", { name: "LOGIN" });
+
+      userEvent.type(usernameInput, username);
+      userEvent.type(passwordInput, password);
+
+      userEvent.click(loginButton);
+
+      expect(mockDispatch).toHaveBeenCalled();
     });
   });
 });
