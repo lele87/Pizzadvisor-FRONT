f1594cb524a8a0b03d2250d95e6ce8e1b47a6303
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-02 23:28:40 +0200

    Added load pizzerias thunk test

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+import mockPizzerias from "./mockPizzerias";
 
 const mockNewUser = {
   username: "lillo",
@@ -18,4 +19,10 @@ export const handlers = [
   rest.post(`${process.env.REACT_APP_API_URL}user/login`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ token: mockToken }));
   }),
+  rest.get(
+    `${process.env.REACT_APP_API_URL}pizzerias/list`,
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(mockPizzerias));
+    }
+  ),
 ];
