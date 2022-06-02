a7d09626a386018cdfd5e4210f3b2dd1862b534f
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-02 23:46:19 +0200

    Created pizzeria list basic css structure

--- src/components/PizzeriaList/PizzeriaList.tsx
+++ src/components/PizzeriaList/PizzeriaList.tsx
@@ -1,15 +1,18 @@
 import { useAppSelector } from "../../redux/store/hooks";
 import Pizzeria from "../Pizzeria/Pizzeria";
+import StyledPizzeriaList from "./StyledPizzeriaList";
 
 const PizzeriaList = () => {
   const pizzerias = useAppSelector((state) => state.pizzerias);
 
   return (
-    <ul>
-      {pizzerias.map((pizzeria) => {
-        return <Pizzeria key={pizzeria.owner} pizzeria={pizzeria} />;
-      })}
-    </ul>
+    <StyledPizzeriaList>
+      <ul>
+        {pizzerias.map((pizzeria) => {
+          return <Pizzeria key={pizzeria.owner} pizzeria={pizzeria} />;
+        })}
+      </ul>
+    </StyledPizzeriaList>
   );
 };
 
