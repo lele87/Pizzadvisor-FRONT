eafe5ee04d9cc753a1d5a021fcdf8273ee898ae0
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-03 13:05:46 +0200

    Implemented logout action onClick

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { logoutActionCreator } from "../../redux/features/userSlice";
+import { useAppDispatch } from "../../redux/store/hooks";
 import StyledHeader from "./StyledHeader";
 
 const Header = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    dispatch(logoutActionCreator());
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
     <>
       <StyledHeader>
         <h1 className="logo-title">Pizzadvisor</h1>
-        <button className="logout-button">LOGOUT</button>
+        <button className="logout-button" onClick={logOut}>
+          LOGOUT
+        </button>
       </StyledHeader>
     </>
   );
