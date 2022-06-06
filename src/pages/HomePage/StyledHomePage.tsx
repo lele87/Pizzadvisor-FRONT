ea88b1684bc10bb6de692af5994a4ca62820526a
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-06 20:48:24 +0200

    Changed info-text font-family in Homepage

--- src/pages/HomePage/StyledHomePage.tsx
+++ src/pages/HomePage/StyledHomePage.tsx
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 
 const StyledHomePage = styled.div`
+  font-family: "Roboto";
+
   .info-text h2 {
     text-align: center;
     letter-spacing: 3px;
-    font-size: larger;
-    font-family: "Mukta", sans-serif;
+    font-weight: 400;
+    font-size: 30px;
   }
 
   .info-text h3 {
+    font-size: 21px;
+    font-weight: 400;
     text-align: center;
     letter-spacing: 3px;
-    font-size: 16px;
-    font-family: "Mukta", sans-serif;
     padding-bottom: 5px;
   }
 `;
