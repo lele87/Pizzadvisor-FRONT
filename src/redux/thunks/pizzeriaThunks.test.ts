958b942d3a0c354c8f13288d06cb19322fb19444
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-07 22:34:46 +0200

    Tested loadPizzeria thunk

---
+++ src/redux/thunks/pizzeriaThunks.test.ts
@@ -0,0 +1,29 @@
+import { mockPizzerias } from "../../mocks/mockPizzerias";
+import { server } from "../../mocks/server";
+import { loadPizzeriaActionCreator } from "../features/pizzeriaSlice";
+import { loadPizzeriaThunk } from "./pizzeriaThunks";
+import axios from "axios";
+
+beforeEach(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("Given a loadPizzeriaThunk function", () => {
+  describe("When it's called", () => {
+    test("Then it should dispatch the loadPizzeriaActionCreator", async () => {
+      const dispatch = jest.fn();
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("token");
+      axios.get = jest
+        .fn()
+        .mockResolvedValue({ data: mockPizzerias[0], status: 200 });
+
+      const loadAction = loadPizzeriaActionCreator(mockPizzerias[0]);
+      const thunk = loadPizzeriaThunk(mockPizzerias[0].id);
+
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(loadAction);
+    });
+  });
+});
