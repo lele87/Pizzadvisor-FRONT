7b396cbbe6043a6120b05cc81b3d0b4ffa6039bf
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-08 09:28:43 +0200

    Fixed EditPizzeriaPage

--- src/pages/EditPizzeriaPage.tsx/EditPizzeriaPage.test.tsx
+++ src/pages/EditPizzeriaPage.tsx/EditPizzeriaPage.test.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import PizzeriaForm from "../../components/PizzeriaForm/PizzeriaForm";
 import store from "../../redux/store";
+import EditPizzeriaPage from "./EditPizzeriaPage";
 
 const mockDispatch = jest.fn();
 
@@ -20,7 +21,7 @@ describe("Given an EditPizzeriaPage", () => {
       render(
         <BrowserRouter>
           <Provider store={store}>
-            <PizzeriaForm />
+            <EditPizzeriaPage />
           </Provider>
         </BrowserRouter>
       );
