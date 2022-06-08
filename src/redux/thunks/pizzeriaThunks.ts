f764398520716316c4e55b358191ca4bf3e7e55a
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-08 09:06:56 +0200

    Added pizzeriaThunks and pizzeriaThunks.test

---
+++ src/redux/thunks/pizzeriaThunks.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { loadPizzeriaActionCreator } from "../features/pizzeriaSlice";
+import { AppDispatch } from "../store";
+
+export const loadPizzeriaThunk =
+  (id: string) => async (dispatch: AppDispatch) => {
+    const url: string = `${process.env.REACT_APP_API_URL}pizzerias/${id}`;
+    const token = localStorage.getItem("token");
+
+    try {
+      const { data: pizzeria } = await axios.get(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (pizzeria) {
+        dispatch(loadPizzeriaActionCreator(pizzeria));
+      }
+    } catch (error: any) {
+      return error.message;
+    }
+  };
