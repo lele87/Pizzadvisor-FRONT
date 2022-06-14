ab7965d1c4f4e860305d428777901a50755aad5c
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-14 09:42:04 +0200

    Added test to PageNotFound

--- src/pages/PageNotFound/PageNotFound.test.tsx
+++ src/pages/PageNotFound/PageNotFound.test.tsx
@@ -1,9 +1,17 @@
 import { Provider } from "react-redux";
 import store from "../../redux/store";
-
 import { BrowserRouter } from "react-router-dom";
 import reactTestRenderer from "react-test-renderer";
 import PageNotFound from "./PageNotFound";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...(jest.requireActual("react-router-dom") as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("Given a PageNotFound Page", () => {
   describe("When it's instantiated", () => {
@@ -17,6 +25,23 @@ describe("Given a PageNotFound Page", () => {
       ).toJSON;
 
       expect(pageNotFound).toMatchSnapshot();
+    });
+  });
+  describe("When the user clicks on the go back homepage button", () => {
+    test("Then it should navigate to Homepage", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PageNotFound />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const goHomePageButton = screen.getByRole("button");
+
+      userEvent.click(goHomePageButton);
+
+      expect(mockedUsedNavigate).toHaveBeenCalled();
     });
   });
 });
