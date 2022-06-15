f8425053fdce50fa7218465a556862211730a748
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-15 12:57:47 +0200

    Fixed Cypress plugins index error

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,10 +13,10 @@
 // the project's config changing)
 
 /**
- * @type {Cypress.PluginConfig}
+ *  @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
