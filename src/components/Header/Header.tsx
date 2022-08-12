661367eef01da06f0c22021671de1e65159bf9ee
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-08-12 13:24:59 +0200

    Added burger menu to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,21 +25,50 @@ const Header = (): JSX.Element => {
 
   const createPizzeria = () => {
     dispatch(blankStateActionCreator());
-    navigate("/createpizzeria");
   };
 
   return (
     <>
-      <StyledHeader>
+      <StyledHeader className="header--container">
+        <div className="hamburger-menu">
+          <input id="menu__toggle" type="checkbox" />
+          <label className="menu__btn" htmlFor="menu__toggle">
+            <span></span>
+          </label>
+
+          <ul className="menu__box">
+            <li>
+              <NavLink className="menu__item" to="/home">
+                <h3>Home</h3>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="menu__item" to="#">
+                <h3>My Favourites places</h3>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="menu__item"
+                to="/createpizzeria"
+                onClick={createPizzeria}
+              >
+                <h3>Add pizzeria</h3>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="menu__item" to="/login" onClick={logOut}>
+                <div className="logout--container">
+                  <h3>Logout</h3>
+                  <i className="fa fa-sign-out"></i>
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         <NavLink className="navlink-home" to="/home">
           <h1 className="logo-title">Pizzadvisor</h1>
         </NavLink>
-        <button className="create-button" onClick={createPizzeria}>
-          CREATE PIZZERIA
-        </button>
-        <button className="logout-button" onClick={logOut}>
-          LOGOUT
-        </button>
       </StyledHeader>
     </>
   );
