8e8a30c9e90297d07a7ff581dd03fead8a422a3a
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-08-12 16:16:04 +0200

    Changed filter css properties

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -21,13 +21,76 @@ const Filter = () => {
     dispatch(filterPizzeriasActionCreator("All"));
   };
   return (
-    <StyledFilter className="filter-buttons">
-      <button onClick={() => showAll()}>ALL</button>
-      <button onClick={() => filterBySpecialty("Diavola")}>DIAVOLA</button>
-      <button onClick={() => filterBySpecialty("Margherita")}>
-        MARGHERITA
-      </button>
-    </StyledFilter>
+    <>
+      <StyledFilter className="filter-details">
+        <details className="custom-select">
+          <summary className="radios">
+            <input
+              type="radio"
+              name="item"
+              id="default"
+              title="Filter for specialty"
+              defaultChecked
+            />
+            <input
+              type="radio"
+              name="item"
+              id="item1"
+              title="All"
+              onClick={() => filterBySpecialty("All")}
+            />
+            <input
+              type="radio"
+              name="item"
+              id="item2"
+              title="Diavola"
+              onClick={() => filterBySpecialty("Diavola")}
+            />
+            <input
+              type="radio"
+              name="item"
+              id="item3"
+              title="Margherita"
+              onClick={() => filterBySpecialty("Margherita")}
+            />
+            <input
+              type="radio"
+              name="item"
+              id="item4"
+              title="4 Cheese"
+              onClick={() => filterBySpecialty("4 Cheese")}
+            />
+            <input
+              type="radio"
+              name="item"
+              id="item5"
+              title="Ham and Mushrooms"
+              onClick={() => filterBySpecialty("Ham and Mushrooms")}
+            />
+          </summary>
+          <ul className="list">
+            <li>
+              <label htmlFor="item1">
+                All
+                <span></span>
+              </label>
+            </li>
+            <li>
+              <label htmlFor="item2">Diavola</label>
+            </li>
+            <li>
+              <label htmlFor="item3">Margherita</label>
+            </li>
+            <li>
+              <label htmlFor="item4">4 Cheese</label>
+            </li>
+            <li>
+              <label htmlFor="item5">Ham and Mushrooms</label>
+            </li>
+          </ul>
+        </details>
+      </StyledFilter>
+    </>
   );
 };
 
